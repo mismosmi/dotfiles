10a6c4db19b2d4da02f19e1906280a1536f8b93b
Michel Smola <m@michelsmola.de>
2017-12-17 12:09:54 +0100

    added setup.sh to clone addons into .vim/bundle

---
+++ .vim/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/.vim/bundle
+git clone https://github.com/chriskempson/base16-vim.git
+git clone https://github.com/termhn/i3-vim-nav.git
+git clone https://github.com/tshirtman/vim-cython.git
+git clone https://github.com/fatih/vim-go.git
